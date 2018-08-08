587c689202fbeee51ed2a1777b9f3807c1c907e9
Ethan Sonza <ethanjericho@yahoo.com>
2018-08-08 12:05:17 +0800

    Kue scheduler

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,11 @@
 var boom = require('boom');
 var getUser = require('./user');
+var kue = require('kue-scheduler'),
+    Queue = kue.createQueue();
+
+const schedTwoMins = '*/5 * * * * *' // https://crontab.guru/every-2-minutes
+var job = Queue.createJob('userCont')
+            .attempts(10).removeOnComplete('true');
 
 var routes = function(app) {
     // View message on /
@@ -16,6 +22,28 @@ var routes = function(app) {
         });
     });
 
+    // Kue scheduler job execute ever 2 minutes
+    app.get('/user_profile_continue', function (req, res, next) {
+        const username = req.query.username;
+        var count = 0;
+        getUser(username, (data) => {
+            data.isBoom ? next(data) : res.status(200).send(data);
+            Queue.process('userCont', function (job, done) {
+                console.log('After 2 minutes...');
+                console.log(data);
+                count++;
+                done();
+            })
+        });
+
+        Queue.on('schedule success', function(job) {
+            if(count==10) Queue.remove(job.id, (err, res) => console.log("Job Terminated.")); 
+        });
+
+        Queue.every(schedTwoMins, job);
+    })
+
+    // Error handler
     app.use(function (err, req, res, next) {
         return res.status(err.output.statusCode).json(err.output.payload);
     })
